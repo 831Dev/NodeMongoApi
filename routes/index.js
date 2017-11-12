06cf0d556680514c1ac083c9aad30c0ebbef31f0
eric chavez <>
2017-11-11 18:26:35 -0800

    trying new release version

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ var dbError = {
 
 router.get('/', function (req, res) {  
     res.json({
-        app: 'Build Number ' + process.env.vNN
+        app: 'Build Number ' + process.env.HEROKU_RELEASE_VERSION
     });
 });
 
